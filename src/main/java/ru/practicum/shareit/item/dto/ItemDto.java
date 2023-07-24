71ce0106cf7ffab99a728628fb44a7886bf2f302
Benito <Benito0727@yandex.ru>
2023-07-24 22:45:18 +0300

    fix item model

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import ru.practicum.shareit.item.model.Item;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 /**
  * TODO Sprint add-controllers.
@@ -14,16 +15,20 @@ import javax.validation.constraints.NotBlank;
 @Setter
 public class ItemDto extends Item {
 
+    private final Long id;
     @NotBlank
     private final String name;
 
     @NotBlank
     private final String description;
 
+    @NotNull
+    private final Boolean available;
 
     public ItemDto(Item item) {
+        this.id = item.getId();
         this.name = item.getName();
         this.description = item.getDescription();
-
+        this.available = item.getAvailable();
     }
 }
