1d46cf3d8c12e33058fefae46d66051652e64842
Benito <Benito0727@yandex.ru>
2023-08-01 13:49:21 +0300

    feat: update application.properties and pom.xml

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -2,13 +2,23 @@ package ru.practicum.shareit.user.model;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @Data
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
     private long id;
 
+    @Column(name = "user_name")
     private String name;
 
+    @Column(name = "user_email")
     private String email;
 
     public User(Long id, String name, String email) {
